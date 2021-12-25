c83036f290956a91d0e36f9a5530f0d5a79b4f41
FlorentZabsonre <florenzabs@hotmail.fr>
2021-12-25 22:23:02 +0100

    avancee

--- SiteVitrine/src/app/app.module.ts
+++ SiteVitrine/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavigationBarComponent,
+    FooterComponent,
+    AboutComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
